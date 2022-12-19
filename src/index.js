de0ccdb4e49c2f7e8d1c69a45338f9766b4282b0
shahul493 <shahul493@gmail.com>
2022-12-19 16:13:38 -0500

    initial count was changed to 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App initialCount={1} />
+    <App initialCount={2} />
   </React.StrictMode>
 );
 
